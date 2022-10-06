6ae7d46ee3d1770d98fbcc62d7d95c49cd24d71c
nndeto <86444679+nndeto@users.noreply.github.com>
2022-10-06 00:01:42 -0400

    began linked list implementation

---
+++ javaScriptDataImplementations/linkedlist.js
@@ -0,0 +1,13 @@
+// Linear Data Structure
+// METHODS
+    //appendData --> adds nodes to the LL
+    //print --> look at the LL
+    //insertAt(index) --> insert node at a specific index
+    //removeFrom(index) --> removes node at a specific index
+    //getNode(index) --> returns node from specified index
+    //clear --> empties a LL by making head point towards null
+//REAL LIFE EXAMPLES
+    // Undo, back button, command-z
+// GENERAL
+    //Dynamic growing or shrinking based on user requirements
+    //Constant time insertion and Deletion
