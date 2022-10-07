8f61885d484b4866ae0d85be77184513912ac1cc
nndeto <86444679+nndeto@users.noreply.github.com>
2022-10-06 20:53:53 -0400

    changes

--- javaScriptDataImplementations/linkedlist.js
+++ javaScriptDataImplementations/linkedlist.js
@@ -11,3 +11,17 @@
 // GENERAL
     //Dynamic growing or shrinking based on user requirements
     //Constant time insertion and Deletion
+    //Can only be accessed in sequence from bebinning to end
+
+    class Node { //nodes are the data and pointer of what's next 
+        constructor(data) {
+            this.data = data
+            this.next = null //starts out as null so that it's always the tail node unless another node is added to the LL
+        }
+    }
+
+    class LinkedList {
+        constructor() {
+            this.head = null;
+        }
+    }
