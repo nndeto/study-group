7bd30a6730d9f3980c233f1b4476d10abef0f672
sagarrat7 <sagarrat7@gmail.com>
2022-10-17 23:15:34 -0700

    add valid parentheses problem

--- neetcode-problems.js
+++ neetcode-problems.js
@@ -87,3 +87,53 @@
                 }
             }
         };
+
+
+        /* Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', 
+        determine if the input string is valid.
+
+        An input string is valid if:
+        
+            Open brackets must be closed by the same type of brackets.
+            Open brackets must be closed in the correct order.
+            Every close bracket has a corresponding open bracket of the same type.
+        
+            https://leetcode.com/problems/valid-parentheses/ 
+        */
+         
+        var isValid = function(s) {
+            // converts string s to an array of characters
+            let arr = s.split("")
+            let stack = [];
+            //console.log(stack)
+        
+          /*  ([)]  [()]
+        
+            push (
+            push [
+            pop )
+            pop ]
+        
+            */
+        
+            for (let i = 0; i < arr.length; i++) {
+                let curr = arr[i];
+                if (curr === "(" || curr === "{" || curr === "[") {
+                    stack.push(curr)
+                } else {
+                    let peekIndx = stack.length - 1;
+                    if (curr === ")" && stack[peekIndx] === "(" ||
+                        curr === "}" && stack[peekIndx] === "{" || 
+                        curr === "]" && stack[peekIndx] === "[") {
+                        stack.pop()
+                    } else {
+                        return false;
+                    }
+                } 
+            }
+        
+            if (stack.length === 0) {
+                return true;
+            }
+            return false;
+        }       
